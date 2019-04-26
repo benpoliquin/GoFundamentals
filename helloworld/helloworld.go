721fd8ee8398ee69c7cbc62bf5b5bfcef5807f3e
Ben Poliquin <ben.poliquin@getcruise.com>
2019-04-26 11:10:03 -0700

    hellowroold

--- helloworld/helloworld.go
+++ helloworld/helloworld.go
@@ -3,9 +3,14 @@ package main
 import "fmt"
 
 func Hello() string {
-    return "hello, world"
+	return "hello, world"
+}
+
+func Hi(name string) string {
+	return "hello, " + name
 }
 
 func main() {
 	fmt.Println(Hello())
+	fmt.Println(Hi("ben"))
 }
--- helloworld/helloworld_test.go
+++ helloworld/helloworld_test.go
@@ -3,11 +3,18 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-    got := Hello()
-    want := "hello, world"
+	got := Hello()
+	want := "hello, world"
 
-    if got != want {
-        t.Errorf("got '%s' want '%s'", got, want)
-    }
+	if got != want {
+		t.Errorf("got '%s' want '%s'", got, want)
+	}
+
+	hi := Hi("ben")
+	should := "hello, ben"
+
+	if hi != should {
+		t.Errorf("got '%s' want '%s'", hi, should)
+	}
+
 }
-
