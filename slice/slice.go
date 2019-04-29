2647edb908fe4928e33eeb2998996555d9b4c3a4
Ben Poliquin <ben.poliquin@getcruise.com>
2019-04-29 10:38:35 -0700

    slicing and suming slices

--- slice/slice.go
+++ slice/slice.go
@@ -9,3 +9,29 @@ func Sum(numbers []int) int {
 	}
 	return total
 }
+
+// SumAll will take a varying number of slices, returning a new slice containing the totals for each slice passed in
+func SumAll(numbersToSum ...[]int) []int {
+	lengthOfNumbers := len(numbersToSum)
+	sums := make([]int, lengthOfNumbers)
+
+	for _, numbers := range numbersToSum {
+		// sums[_] = Sum(numbers)
+    sums = append(sums, Sum(numbers))
+	}
+	return sums
+}
+
+// SumAllTails returns the sum of all tails(everything but the firtst element) of each slice
+func SumAllTails(numbersToSum ...[]int) []int {
+    var sums []int
+    for _, numbers := range numbersToSum {
+        if len(numbers) == 0 {
+            sums = append(sums, 0)
+        } else {
+            tail := numbers[1:]
+            sums = append(sums, Sum(tail))
+        }
+    }
+    return sums
+}
